4a37cb9957772680d375f028fd9059198b7e13dd
ivai <ismb9@hotmail.com>
2022-08-29 17:48:06 -0500

    Update main.py

--- AprendizajeSinFrameWork/A01114959/main.py
+++ AprendizajeSinFrameWork/A01114959/main.py
@@ -1,5 +1,7 @@
 from bruteForceLinearRegression import bruteForceLinearRegression as bflr
 
+#WTF is this
+
 model = bflr()
 model.loadData(R'C:\Users\omega\OneDrive\Documents\GitHub\ConcentracionIA\AprendizajeSinFrameWork\A01114959\data.csv')
 model.numericLinearRegression()
